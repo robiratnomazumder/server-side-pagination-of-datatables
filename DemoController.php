88fe38c841165d6c3c7820b8c4ec57a0cd338be5
robi ratno mazumder <35422203+robiratnomazumder@users.noreply.github.com>
2020-01-28 17:53:12 +0600

    updated code

--- DemoController.php
+++ DemoController.php
@@ -1,1 +1,14 @@
-demo controller....
+
+
+"columns": [
+        // {"data": "id"},
+        {"data": "song"},
+        // {"data": "singer"},
+        // {"data": "last_modify_date"},
+        // {"data": "created"},
+        // {
+        //     "data": null,
+        //     "defaultContent": '<button type="button" class="btn btn-info">Edit</button>' + '&nbsp;&nbsp' +
+        //     '<button type="button" class="btn btn-danger">Delete</button>'
+        // }
+    ]
